bec07a039609efb3a880de743336e98a8de75fe3
Fernando Goncalves <fernando.hmg@gmail.com>
2021-10-01 20:41:39 -0300

    Fixing input component

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -1,50 +1,77 @@
 import React from "react";
 import styled from "styled-components";
 import { COLORS } from "../../constants";
+import Icon from "../Icon";
+import VisuallyHidden from "../VisuallyHidden";
 
 const SIZE = {
   small: {
-    "--border-width": `${1}px`,
-    "--font-size": `${14}px`,
-    "--line-height": `${16}px`,
-    "--icon-size": `${10}px`,
+    fontSize: 14,
+    iconSize: 16,
+    borderThickness: 1,
+    height: 24,
   },
   large: {
-    "--border-width": `${2}px`,
-    "--font-size": `${18}px`,
-    "--line-height": `${21}px`,
-    "--icon-size": `${16}px`,
+    fontSize: 18,
+    iconSize: 24,
+    borderThickness: 2,
+    height: 36,
   },
 };
 
-const IconInput = ({ label, icon, width = 250, size, placeholder }) => {
+const IconInput = ({ label, icon, width = 250, size, ...delegated }) => {
   const style = SIZE[size];
   return (
-    <Input
-      style={style}
-      label={label}
-      width={width}
-      placeholder={placeholder}
-    />
+    <Wrapper>
+      <VisuallyHidden>{label}</VisuallyHidden>
+      <IconWrapper style={{ "--size": style.iconSize + "px" }}>
+        <Icon id={icon} size={style.iconSize} />
+      </IconWrapper>
+      <TextInput
+        {...delegated}
+        style={{
+          "--width": width + "px",
+          "--height": style.height + "px",
+          "--border-thickness": style.borderThickness + "px",
+          "--font-size": style.fontSize + "px",
+        }}
+      />
+    </Wrapper>
   );
 };
 
-const Input = styled.input`
-  width: ${(p) => p.width}px;
-  border: transparent;
-  border-bottom: var(--border-width) solid ${COLORS.black};
+const Wrapper = styled.label`
+  display: block;
+  position: relative;
   color: ${COLORS.gray700};
+
+  &:hover {
+    color: ${COLORS.black};
+  }
+`;
+
+const IconWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  margin: auto;
+  height: var(--size);
+`;
+
+const TextInput = styled.input`
+  width: var(--width);
+  height: var(--height);
   font-size: var(--font-size);
-  font-weight: bold;
-  outline-offset: 4px;
+  border: none;
+  border-bottom: var(--border-thickness) solid ${COLORS.black};
+  padding-left: var(--height);
+  color: inherit;
+  font-weight: 700;
+  outline-offset: 2px;
 
   &::placeholder {
     color: ${COLORS.gray500};
-    font-weight: normal;
-  }
-
-  &:focus {
-    color: ${COLORS.black};
+    font-weight: 400;
   }
 `;
 
