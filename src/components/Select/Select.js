eebab9de79376080d41f22fd7aefc7ec15a938e4
Fernando Goncalves <fernando.hmg@gmail.com>
2021-10-01 19:10:40 -0300

    Fixing implementation

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -9,42 +9,60 @@ const Select = ({ label, value, onChange, children }) => {
 
   return (
     <Wrapper>
-      <SelectComponent value={value} onChange={onChange}>
+      <NativeSelect value={value} onChange={onChange}>
         {children}
-      </SelectComponent>
-      <IconWrapper id="chevron-down" size={16} />
+      </NativeSelect>
+      <PresentationalBit>
+        {displayedValue}
+        <IconWrapper style={{ "--size": 24 + "px" }}>
+          <Icon id="chevron-down" strokeWidth={1} size={24} />
+        </IconWrapper>
+      </PresentationalBit>
     </Wrapper>
   );
 };
 
 const Wrapper = styled.div`
   position: relative;
-  width: fit-content;
-  color: ${COLORS.gray700};
-  &:hover {
-    color: ${COLORS.black};
-  }
+  width: max-content;
 `;
 
-const SelectComponent = styled.select`
+const NativeSelect = styled.select`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+`;
+
+const PresentationalBit = styled.div`
+  color: ${COLORS.gray700};
   background-color: ${COLORS.transparentGray15};
-  color: inherit;
+  font-size: ${16 / 16}rem;
   padding: 12px 16px;
   padding-right: 52px;
   border-radius: 8px;
-  width: fit-content;
-  height: 43px;
-  border: none;
-  appearance: none;
+
+  ${NativeSelect}:focus + & {
+    outline: 1px dotted #212121;
+    outline: 5px auto -webkit-focus-ring-color;
+  }
+
+  ${NativeSelect}:hover + & {
+    color: black;
+  }
 `;
 
-const IconWrapper = styled(Icon)`
-  font-weight: 700;
+const IconWrapper = styled.div`
   position: absolute;
   top: 0;
   bottom: 0;
+  right: 10px;
   margin: auto;
-  left: 80%;
+  width: var(--size);
+  height: var(--size);
+  pointer-events: none;
 `;
 
 export default Select;
